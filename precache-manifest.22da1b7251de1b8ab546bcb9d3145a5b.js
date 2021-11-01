36957b461f8a2d505fef53f55035fef6c6dcf543
Tahir <58627780+Mh-Tahir@users.noreply.github.com>
2021-11-01 21:08:16 +0300

    Add files via upload

---
+++ precache-manifest.22da1b7251de1b8ab546bcb9d3145a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79fb8002962df2fa01bf0689b9395d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7d2cd32b7d16db4cc28",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8ae49aac9dab507a9ba8",
+    "url": "/static/js/2.18b2a2a8.chunk.js"
+  },
+  {
+    "revision": "f7d2cd32b7d16db4cc28",
+    "url": "/static/js/main.53dcd900.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
